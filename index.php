8e18537b3f10699377b412c6075480f03124bab9
carlwatson <carl.erik.watson@gmail.com>
2016-05-19 17:32:50 -0500

    fix error in index.php file

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
+<!DOCTYPE html >
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="src/css/theme.css"/>
+  <script type="application/javascript" src="src/js/canvas.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+  <script src="src/js/toolbar.js"></script>
+  <script src="vendor/tracking.js/build/tracking-min.js"></script>
+</head>
+<body>
 
--<!DOCTYPE html >
--<html>
--<head>
-  -<link rel="stylesheet" type="text/css" href="src/css/theme.css" />
-  -<script type="application/javascript" src="src/js/canvas.js"></script>
-  -</head>
--<body>
--
--<h1>Canvas!</h1>
--<canvas id="canvas">
-  -
-  -</canvas>
--
--
--
--
--<form action="upload.php" method="post" enctype="multipart/form-data">
-  -	Select image to upload:
-  -	<input type="file" name="fileToUpload" id="fileToUpload">
-  -	<input type="submit" value="Upload Image" name="submit">
-  -</form>
--</body>
--<footer>
-  -
-  -</footer>
--</html> 
+<h1>Canvas!</h1>
+<canvas id="canvas" height="600px" width="800px">
+</canvas>
+
+<div id="tools">
+  <button id="pen-button">Pen</button>
+  <button id="camera-button">Camera</button>
+</div>
+
+<div id="content-window">
+  <img id="myImg" src="https://www.google.com/logos/doodles/2016/yuri-kochiyamas-95th-birthday-5723472594468864-hp.jpg"/>
+  <video id="cameraFeed" width="400" height="300" style="display: none" preload autoplay loop muted></video>
+</div>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+<script type="text/javascript">
+  img = document.getElementById('myImg');
+  canvas = document.getElementById('canvas');
+  editCan(canvas, img);
+</script>
+
+<script src="src/js/tracking/colorTracker.js"></script>
+</body>
+<footer>
+
+</footer>
+</html>
