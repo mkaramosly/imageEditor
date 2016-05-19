7af92fa90e3b35ea44c26847de61ccc20fc5787a
Mehdi Karamosly <mehdi.karamosly@breaktech.com>
2016-05-19 16:10:44 -0500

    Some changes to draw the picture on the canvas and draw on the canvas works on firefox (touches only work in chrome)

--- index.php
+++ index.php
@@ -7,18 +7,27 @@
 <body>
 
 <h1>Canvas!</h1>
-<canvas id="canvas">
+<canvas id="canvas" height="600px" width="800px">
 
 </canvas>
 
 
-
+<img id="myImg" src="https://www.google.com/logos/doodles/2016/yuri-kochiyamas-95th-birthday-5723472594468864-hp.jpg"/>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
 	Select image to upload:
 	<input type="file" name="fileToUpload" id="fileToUpload">
 	<input type="submit" value="Upload Image" name="submit">
 </form>
+
+<script type="text/javascript">
+img = document.getElementById('myImg');
+canvas = document.getElementById('canvas');
+
+editCan(canvas, img);
+
+</script>
+
 </body>
 <footer>
 
