5bcffb80be189fa42bad94c3d2ac4661487d61e3
Kendall Parks <kendall.parks@breaktech.com>
2016-05-19 16:41:53 -0500

    Hiding camera feed by default. Added 'Pen' and 'Camera' buttons. Camera feed initializes when 'Camera' is clicked.

--- index.php
+++ index.php
@@ -11,9 +11,15 @@
 <canvas id="canvas" height="600px" width="800px">
 </canvas>
 
+<div id="tools">
+    <button id="pen-button">Pen</button>
+    <button id="camera-button">Camera</button>
+</div>
 
-<img id="myImg" src="https://www.google.com/logos/doodles/2016/yuri-kochiyamas-95th-birthday-5723472594468864-hp.jpg"/>
-
+<div>
+    <img id="myImg" src="https://www.google.com/logos/doodles/2016/yuri-kochiyamas-95th-birthday-5723472594468864-hp.jpg"/>
+    <video id="cameraFeed" width="400" height="300" style="display: none" preload autoplay loop muted></video>
+</div>
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
@@ -21,15 +27,12 @@
 </form>
 
 <script type="text/javascript">
-img = document.getElementById('myImg');
-canvas = document.getElementById('canvas');
+    img = document.getElementById('myImg');
+    canvas = document.getElementById('canvas');
 
-editCan(canvas, img);
-
+    editCan(canvas, img);
 </script>
 
-
-<video id="cameraFeed" width="400" height="300" preload autoplay loop muted></video>
 <script src="src/js/tracking/colorTracker.js"></script>
 </body>
 <footer>
--- src/js/tracking/colorTracker.js
+++ src/js/tracking/colorTracker.js
@@ -1,13 +1,26 @@
-var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+var showCameraFeed = function() {
+    document.getElementById('cameraFeed').style.display = 'block';
+};
 
-colors.on('track', function(event) {
-    if (event.data.length === 0) {
-        // No colors were detected in this frame.
-    } else {
-        event.data.forEach(function(rect) {
-            console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
-        });
-    }
-});
+var hideCameraFeed = function() {
+    document.getElementById('cameraFeed').style.display = 'none';
+};
 
-tracking.track('#cameraFeed', colors, { camera: true });
+// Initializes the camera feed.
+document.getElementById('camera-button').onclick = function () {
+    var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+
+    colors.on('track', function (event) {
+        if (event.data.length === 0) {
+            // No colors were detected in this frame.
+        } else {
+            event.data.forEach(function (rect) {
+                console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+            });
+        }
+    });
+
+    tracking.track('#cameraFeed', colors, {camera: true});
+    showCameraFeed();
+};
+
